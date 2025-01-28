80677e0668c1de67b906676017fc0fce4385d2a5
frandd07 <fran.doblado.diaz@gmail.com>
2025-01-28 10:53:22 +0100

    Update boletin completo

--- src/app/components/Grid.js
+++ src/app/components/Grid.js
@@ -1,14 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Grid() {
   return (
-    <div className="container">
-      <h2 className="text-center">Ejercicio 2</h2>
-      <div className="row">
-        <div className="col-md-3">Columna 1 (3 columnas)</div>
+    <div className="container my-5">
+      <h2 className="text-center mb-4 text-dark">Ejercicio 2</h2>
+      <div className="row mb-4">
+        <div className="col-md-3 p-3 bg-light border rounded shadow-sm">
+          <h5 className="text-center text-secondary">Columna 1 (3 columnas)</h5>
+          <p className="text-center">Primera columna</p>
+        </div>
       </div>
       <div className="row">
-        <div className="col-md-8">Columna 1 (8 columnas)</div>
-        <div className="col-md-4">Columna 1 (4 columnas)</div>
+        <div className="col-md-8 p-3 bg-danger text-white rounded shadow-sm">
+          <h5 className="text-center">Columna 1 (8 columnas)</h5>
+          <p>Contenido de la segunda columna.</p>
+        </div>
+        <div className="col-md-4 p-3 bg-success text-white rounded shadow-sm">
+          <h5 className="text-center">Columna 2 (4 columnas)</h5>
+          <p>Contenido de la tercera columna.</p>
+        </div>
       </div>
     </div>
   );
